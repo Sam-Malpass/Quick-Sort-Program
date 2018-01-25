8e23e4e3af296077f3c4744aeb632a2be43a899b
Sam <sam@malpass.me.uk>
2018-01-25 21:26:28 +0000

    Add base GUI functions

--- src/quickSort/GUI.java
+++ src/quickSort/GUI.java
@@ -1,5 +1,61 @@
 package quickSort;
 
-public class GUI {
+import java.util.ArrayList;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+	private ArrayList<Double> data;
+	public void sortingWindow()
+	{
+		
+	}
+	public HBox createButtons()
+	{
+		HBox buttons = new HBox();
+		Button save = new Button("Save");
+		save.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent actionEvent) {
+				String w = "";
+				for(int i = 0; i < data.size(); i++) {
+					w = w + data.get(i) + " ";
+				}
+				FileIO.save(w);
+			}
+		});
+		Button load = new Button("Load");
+		load.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent actionEvent) {
+				data = FileIO.load();
+				sortingWindow();
+			}
+		});
+		Button exit = new Button("Exit");
+		exit.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent actionEvent) {
+				System.exit(0);
+			}
+		});
+		return buttons;
+	}
+	public void start(Stage stage) throws Exception {
+		Stage mainStage = stage;
+		mainStage.setTitle("QuickSort Program");
+		HBox buttonBox = createButtons(); 
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(buttonBox);
+		Scene mainScene = new Scene(borderPane, 300, 100);
+		mainStage.setScene(mainScene);
+		mainStage.show();
+	}
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 }
