6d218bcbcf6b1d619888957ce8f0b1b51732517e
Sam <sam@malpass.me.uk>
2018-01-25 21:07:11 +0000

    Add load function to FileIO

--- src/quickSort/FileIO.java
+++ src/quickSort/FileIO.java
@@ -1,9 +1,12 @@
 package quickSort;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.ArrayList;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
@@ -61,5 +64,58 @@ public class FileIO {
 		{
 			return;
 		}
+	}
+	/**
+	 * Function definition for load()
+	 * <p>
+	 * Opens a file, reads a string from said file and then writes the numbers
+	 * to an ArrayList.
+	 * <p>
+	 * @return an ArrayList of Doubles
+	 */
+	public static ArrayList<Double> load() {
+		/*Create an ArrayList of doubles*/
+		ArrayList<Double> inputData = new ArrayList<Double>();
+		/*Create a JFrame*/
+		JFrame frame = new JFrame();
+		/*Create a JFileChooser*/
+		JFileChooser loadFrom = new JFileChooser();
+		/*Sets the title of the JFileChooser*/
+		loadFrom.setDialogTitle("Load From");
+		/*Create a variable and set it to the value of the user input*/
+		int option = loadFrom.showOpenDialog(frame);
+		/*If the option chosen is confirm*/
+		if(option == JFileChooser.APPROVE_OPTION)
+		{
+			/*Open the user specified file*/
+			File file = loadFrom.getSelectedFile();
+			/*Create a FileReader*/
+			try (FileReader fileReader = new FileReader(file))
+			{
+				/*Create a BufferedReader*/
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				/*Create a string for the raw read*/
+				String rawRead = bufferedReader.readLine();
+				/*Splits the string at spaces*/
+				String[] processedRead = rawRead.split(" ");
+				/*For all the values in the string array*/
+				for(int i = 0; i < processedRead.length; i++) {
+					/*Parse the data in the array to an ArrayList*/
+					inputData.add(Double.parseDouble(processedRead[i]));
+				}
+				/*Close the file*/
+	            bufferedReader.close();
+			} 
+			catch (FileNotFoundException exception) 
+			{
+				exception.printStackTrace();
+			} 
+			catch (IOException exception) 
+			{
+				exception.printStackTrace();
+			}
+		}
+		/*Return the ArrayList*/
+		return inputData;
 	}
 }
